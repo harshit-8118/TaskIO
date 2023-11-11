9709e4b15d8f0445931b8ee2dbd17da615687552
harshit shukla <hharshit8118@gmail.com>
2023-11-11 14:29:58 +0530

    added featured

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import TaskComp from "../../components/TaskComp";
+import Featured from "../../components/Featured";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <TaskComp />
-
+      <Featured />
     </div>
   );
 }
