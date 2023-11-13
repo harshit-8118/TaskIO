2867e2b3def34c7f0fee347db3779ac41e98fdbe
harshit shukla <hharshit8118@gmail.com>
2023-11-13 18:22:01 +0530

    configured firebase/storage

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_ADDNOTE_API_KEY,
+  authDomain: "addnote-1699259210502.firebaseapp.com",
+  projectId: process.env.REACT_APP_ADDNOTE_PROJECT_ID,
+  storageBucket: "addnote-1699259210502.appspot.com",
+  messagingSenderId: process.env.REACT_APP_ADDNOTE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ADDNOTE_MESSAGING_APP_ID,
+  measurementId: "G-6HSXHRG2FJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+export default storage;
